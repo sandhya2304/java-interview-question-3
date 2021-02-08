9c36396f126b43656c6b95be5fb6ac07228f71a8
sandhya2304 <sandhyasharma0423@gmail.com>
2021-02-09 03:43:16 +0530

    design pattern

--- src/feb6/AverageNumber.java
+++ src/feb6/AverageNumber.java
@@ -1,10 +1,31 @@
 package feb6;
 
-public class AverageNumber {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class AverageNumber
+{
+	public static void main(String[] args) 
+	{
+		
+		int marks[] = new int[5];
+		
+		float sum=0,average,percentage;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the marks :");
+	      
+		for(int i=0;i<5;i++)
+		{
+			marks[i] = sc.nextInt();
+			sum = sum+marks[i];
+			
+		}
+		
+		average = sum/5;
+		percentage = (sum/500)*100;
+		
+		System.out.println("average :" +average);
+		System.out.println("percentgae "+percentage +" % ");
 	}
 
 }
